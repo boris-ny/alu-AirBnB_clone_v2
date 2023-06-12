d579f66cc954d095bd4d2cbfd46108a566f96d6c
boris-ny <b.nyilindek@alustudent.com>
2023-06-12 19:18:06 +0200

    Task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,7 +61,7 @@ class FileStorage:
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
-                self.__objects.pop[key]
+                self.__objects.pop(key)
                 self.save()
             else:
                 pass

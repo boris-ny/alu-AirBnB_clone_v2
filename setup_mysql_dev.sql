62546c65a228b5a906f6a0fc956a9c3c74dc1df0
boris-ny <b.nyilindek@alustudent.com>
2023-06-12 19:02:49 +0200

    updating database script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 --Prepares a MySqL server for the development environment
 
-CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON perfomance_schema. * TO 'hbnb_dev'@'localhost';

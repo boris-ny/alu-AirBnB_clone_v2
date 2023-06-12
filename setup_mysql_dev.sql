cdeef6f1c31c0626baf6a093a2e1b7d5670dd017
boris-ny <b.nyilindek@alustudent.com>
2023-06-12 19:45:09 +0200

    creating database engine

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,4 @@
 --Prepares a MySqL server for the development environment
-
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';

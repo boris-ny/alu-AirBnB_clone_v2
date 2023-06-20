46306e0928a7dbc52d0eebfad40f1a77559c2618
boris-ny <b.nyilindek@alustudent.com>
2023-06-21 00:21:44 +0200

    second task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive from the contents of the webs_static folder of your AirBnB Clone repo, using the function do_pack."""
+"""Fabric script that generates a .tgz archive from the contents of the
+web_static folder of your
+AirBnB Clone repo, using
+the function do_pack."""
 
 from fabric.api import local
 from datetime import datetime
 import os
 
+
 def do_pack():
-    """Function to generate a .tgz archive from the contents of the web_static folder."""
+    """Function to generate a .tgz archive from the contents of the web_static
+    folder."""
 
     time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
     local("mkdir -p versions")

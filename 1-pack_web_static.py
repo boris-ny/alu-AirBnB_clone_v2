603c422fbfa3ef6709d8881a52223472d0a9131a
boris-ny <b.nyilindek@alustudent.com>
2023-06-21 00:19:21 +0200

    second task

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tgz archive from the contents of the webs_static folder of your AirBnB Clone repo, using the function do_pack."""
+
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+    """Function to generate a .tgz archive from the contents of the web_static folder."""
+
+    time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    local("mkdir -p versions")
+    file_name = "versions/web_static_{}.tgz".format(time_stamp)
+    local("tar -cvzf {} web_static".format(file_name))
+    if os.path.exists(file_name):
+        return file_name
+    else:
+        return None

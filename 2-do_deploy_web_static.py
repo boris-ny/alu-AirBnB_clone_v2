fa1037d1c5413008644e47e4a7fab2e82b5072af
boris-ny <b.nyilindek@alustudent.com>
2023-06-21 02:53:28 +0200

    3rd task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Fabric script that distributes an archive to your web servers"""
-from genericpath import isdir
-from fabric.api import *
-import os
-import re
+from fabric.api import env, put, run
+from os.path import exists
 
 env.hosts = ["54.226.54.247", "18.205.246.150"]
 env.user = "ubuntu"
@@ -12,24 +10,20 @@ env.key = "~/.ssh/id_rsa"
 
 def do_deploy(archive_path):
     """Function to distribute an archive to your web servers"""
-    if not os.path.exists(archive_path):
+    if not exists(archive_path):
         return False
-
-    put(archive_path, "/tmp/")
-    filename = re.search(r'[^/]+$', archive_path).group(0)
-    folder = "/data/web_static/releases/{}/".format(os.path.splitext(
-        filename)[0])
-    run("mkdir -p {}".format(folder))
-    run("tar -xzf /tmp/{} -C {}".format(filename, folder))
-    run("rm /tmp/{}".format(filename))
-    run("mv {}web_static/* {}".format(folder, folder))
-    run("rm -rf {}web_static".format(folder))
-    run("rm -rf /data/web_static/current")
-    run("ln -s {} /data/web_static/current".format(folder))
-
-    if not isdir("/var/www/html/hbnb_static"):
-        run("mkdir -p /var/www/html/hbnb_static")
-
-    run("sudo cp -r /data/web_static/current/* /var/www/html/hbnb_static/")
-    print("New version deployed")
-    return True
+    try:
+        file_name = archive_path.split("/")[-1]
+        name = file_name.split(".")[0]
+        path_name = "/data/web_static/releases/" + name
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(path_name))
+        run('tar -xzf /tmp/{} -C {}/'.format(file_name, path_name))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {}/web_static/* {}".format(path_name, path_name))
+        run("rm -rf {}/web_static".format(path_name))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}/ /data/web_static/current'.format(path_name))
+        return True
+    except Exception:
+        return False

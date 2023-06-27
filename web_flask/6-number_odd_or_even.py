a8a76130ab2ad36e4359bb4ce2e6c84f3d2aebbb
boris-ny <b.nyilindek@alustudent.com>
2023-06-27 23:58:51 +0200

    pycodestyle

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -4,8 +4,10 @@ Your web application must be listening on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
 /hbnb: display “HBNB”
-/c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
-/python/(<text>): display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
+/c/<text>: display “C ”, followed by the value of the text variable
+(replace underscore _ symbols with a space )
+/python/(<text>): display “Python ”, followed by the value of the text variable
+(replace underscore _ symbols with a space )
 The default value of text is “is cool”
 /number/<n>: display “n is a number” only if n is an integer
 /number_template/<n>: display a HTML page only if n is an integer:

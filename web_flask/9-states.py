51ac48dc10da973cb12f1208ad6116a77b945925
boris-ny <b.nyilindek@alustudent.com>
2023-06-28 01:20:53 +0200

    9-states.html

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -47,10 +47,10 @@ def states():
 @app.route('/states/<id>', strict_slashes=False)
 def states_id(id):
     """Displays an html page with info about id if it exists"""
-    for state in storage.all("State"):
+    for state in storage.all("State").values():
         if state.id == id:
             return render_template('9-states.html', state=state)
-    return render_template('9-states.html', state=None)
+    return render_template('9-states.html')
 
 
 if __name__ == '__main__':
